739d2e13690d3df351ab8ca84305136b65d67559
George Soloviev <george.soulv@gmail.com>
2024-03-11 20:46:56 +0300

    first commit

---
+++ homeTask.js
@@ -0,0 +1,34 @@
+const mySet = new MySet([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+
+// Глава 1, 5. Грокаем алгоритмы
+// Реализовать класс MySet
+// Придумать проект над которым будем работать
+// Читать Грокаем алгоритмы, и https://learn.javascript.ru/map-set
+// Понять что такое Event Loop
+// Знать разницу между let var const
+// learn.javascript.ru :
+// Перавая часть Язык Javascript
+// https://learn.javascript.ru/event-loop
+// https://learn.javascript.ru/dom-nodes
+// https://learn.javascript.ru/script-async-defer
+// https://learn.javascript.ru/constructor-new
+
+class MySet {
+
+  constructor(array) {
+
+  }
+
+  has(value) {
+
+  }
+
+  add(value) {
+
+  }
+
+  remove(value) {
+
+  }
+
+}
